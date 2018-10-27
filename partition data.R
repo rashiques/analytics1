f0bd86c717cf55182490dca5259416b99171792a
rashiques <rashique303.com>
2018-10-27 11:51:30 +0530

    27/10/2018

--- partition data.R
+++ partition data.R
@@ -19,6 +19,7 @@ sname
 gender
 marks
 df=data.frame(sname,gender,marks)
+length(df)# it is the number of column
 head(df)
 nrow(df)
 table(df$gender)
@@ -30,5 +31,32 @@ firstset
 secondset
 nrow(firstset)
 nrow(secondset)
-(sample(10,2,replace =F))#THEY WILL TAKE 1 TO 10 IN THAT TWO NUMBERS IS SELECTED AND NO REPLACEMENT
+#or
+library(caTools)
+split=sample.split(df,SplitRatio = .6)
+head(split)#IT WILL SAY TRUE FOR DATA IN FIRST SET FALSE FOR DATA IN SECOND SET
+nrow(df)
+firset=subset(df,split==T)
+secondset=subset(df,split==F)
+firstset
+secondset#dought
+nrow(firstset)
+nrow(secondset)#dought
+#3rd method
+library(caret)# actually df having 60% male and female 40% so we are dividing into two set that two set should have 60% male and 40% female
+table(df$gender)#number of male and female
+prop.table(table(df$gender))#it will dive you in percentage form
+(index1=createDataPartition(y=df$gender,p=0.7,list=F))#i have to make propotion of gender is fixed and divide set in 70% and 60% don,t print list wise
+length(index1)
+(train1=df[index1,])#70% set
+(test1=df[-index1,])#30% set
+(t1a=table(train1$gender))# number of male and female in train1
+prop.table(t1a)#percentage of male and female in train1 it should 60%male 40%female
+(t1b=table(test1$gender))
+prop.table(t1b)
+
+
+
+(sample(200,2,replace =F))#THEY WILL TAKE 1 TO 10 IN THAT TWO NUMBERS IS SELECTED AND NO REPLACEMENT
+
 
